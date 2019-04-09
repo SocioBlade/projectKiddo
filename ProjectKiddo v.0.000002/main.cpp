587d8edad0770b68ac57b58950253de7400f1537
Tim Lagerqvist <tim.lagerqvist@gmail.com>
2019-04-09 11:11:16 +0200

    Simple Tris

--- ProjectKiddo v.0.000002/main.cpp
+++ ProjectKiddo v.0.000002/main.cpp
@@ -32,6 +32,14 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBegin(GL_TRIANGLES);
+
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(0.0, 0.5);
+		glVertex2f(0.5,	-0.5);
+
+		glEnd();
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
